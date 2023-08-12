7710efe2d31a9b44c32a3e1119a4d7fe6229f775
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2023-08-12 15:15:30 +0300

    start creating templete reused component .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { styled } from "styled-components";
 import GlobleStyles from "./styles/GlobleStyles";
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
 
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
 function App() {
   return (
     <>
--- src/styles/GlobleStyles.js
+++ src/styles/GlobleStyles.js
@@ -87,6 +87,7 @@ textarea,
 select {
   font: inherit;
   color: inherit;
+
 }
 
 button {
